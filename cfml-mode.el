98a479f638a7c8201c3b0cdd0409806a1fcd2301
Andrew Myers <amyers@rise-digital.com.au>
2019-06-15 23:53:38 +1000

    Improved indenting

--- cfml-mode.el
+++ cfml-mode.el
@@ -5,7 +5,7 @@
 ;; Author: Andrew Myers <am2605@gmail.com>
 ;; URL: https://github.com/am2605/cfml-mode
 ;; Version: 1.1.0
-;; Package-Requires: ((emacs "25") (mmm-mode "0.5.4") (cftag-mode "1.0.0"))
+;; Package-Requires: ((emacs "25") (mmm-mode "0.5.4"))
 
 ;;{{{ GPL
 
@@ -36,69 +36,118 @@
 
 ;;; Code:
 
-(with-no-warnings
-  (require 'mmm-mode)
-  (require 'cftag-mode))
+(defgroup cfml nil
+  "Major mode for cfml files"
+  :prefix "cfml-"
+  :group 'languages
+  :link '(url-link :tag "Github" "https://github.com/am2605/cfml-mode")
+  :link '(emacs-commentary-link :tag "Commentary" "cfml-mode"))
 
-;;;###autoload
-(add-to-list 'magic-mode-alist
-             '("<cfcomponent" . cftag-mode))
-;;;###autoload
-(add-to-list 'magic-mode-alist
-             '("<!---" . cftag-mode))
-;;;###autoload
-(add-to-list 'auto-mode-alist
-             '("\\.cfm\\'" . cftag-mode))
-;;;###autoload
-(add-to-list 'auto-mode-alist
-             '("\\.cfc\\'" . cfml-cfscript-mode))
+(defcustom cfml-tab-width 4
+  "Tab width for cfml-mode."
+  :group 'cfml
+  :type 'integer)
 
-(setq mmm-global-mode 'maybe)
-;;;###autoload
-(mmm-add-mode-ext-class nil "\\.cfm\\'" 'cfml-cftag)
-;;;###autoload
-(mmm-add-mode-ext-class nil "\\.cfc\\'" 'cfml-cftag)
-;;;###autoload
-(mmm-add-mode-ext-class nil "\\.cfm\\'" 'cfml-js)
-(setq mmm-submode-decoration-level 0)
+(defcustom cfml-extra-indent 0
+  "The number of columns added to every line's indentation."
+  :group 'cfml
+  :type 'integer)
 
-(mmm-add-classes
- '((cfml-cftag
-    :submode cfml-cfscript-mode
-    :front "<cfscript>"
-    :back "[ \t]*</cfscript>")))
+(defconst cfml-html-empty-tags
+	      '("area" "base" "basefont" "br" "col" "frame" "hr" "img" "input"
+		"isindex" "link" "meta" "param" "wbr"))
 
-(mmm-add-classes
- '((cfml-js
-    :submode js-mode
-    :front "<script[^>]*>[ \t]*\n?"
-    :back "[ \t]*</script>")))
+(defconst cfml-html-unclosed-tags 
+	'("body" "colgroup" "dd" "dt" "head" "html" "li" "option"
+		"p" "tbody" "td" "tfoot" "th" "thead" "tr"))
 
-(defun cfml-indent-to-previous ()
-  "Insert a newline character then indent the new line just like the previous line."
+(defconst cfml-empty-tags
+	'("cfargument" "cfdump" "cfinclude" "cfparam" "cfqueryparam" "cfset" "cfsetting" "cfthrow" ))
+	
+(defconst cfml-unclosed-tags
+	'("cfelse" "cfelseif"))
+
+(defun cfml-last-line-p ()
+  "Return whether point is on the last line in the buffer."
+  (save-excursion (= (line-number-at-pos) (line-number-at-pos (point-max)))))
+
+(defun cfml-line-empty-p ()
+  "Return whether point is on an empty line."
+  (string-match-p "^\s*$" (buffer-substring (point-at-bol) (point-at-eol))))
+
+(defun cfml-get-previous-indentation ()
+  "Get the column of the previous indented line"
   (interactive)
-  (newline)
-  (unless (looking-back "\\`\n*")
-    (indent-relative-maybe)))
+  (save-excursion
+    (progn
+      (move-beginning-of-line nil)
+      (skip-chars-backward "\n \t")
+      (back-to-indentation))
+    (current-column)))
 
+(defconst cfml-outdent-regexp
+	"\\(<cfelse\\(if[^>]+\\)?>\\)"
+)
+
+(defun cfml-indent ()
+  (interactive)
+  (save-excursion
+	(beginning-of-line)
+	(skip-chars-forward " \t")
+	(cond
+	  ((looking-at cfml-outdent-regexp) (indent-line-to (max 0 (- (cfml-get-previous-indentation) cfml-tab-width))))
+	  (t (sgml-indent-line)))))
+	 
 ;;;###autoload
-(define-derived-mode cfml-cfscript-mode js-mode "cfscript"
-  (setq tab-stop-list (number-sequence sgml-basic-offset 120 sgml-basic-offset))
-  (local-set-key (kbd "RET") 'cfml-indent-to-previous)
-  (electric-indent-local-mode -1)
+(define-derived-mode cfml-script-mode js-mode "cfscript"
   (add-to-list 'mmm-save-local-variables 'js--quick-match-re)
   (add-to-list 'mmm-save-local-variables 'js--quick-match-re-func))
 
 ;;;###autoload
-(define-derived-mode cfml-mode cftag-mode "CFML"
-  (setq tab-stop-list (number-sequence sgml-basic-offset 120 sgml-basic-offset))
-  (local-set-key (kbd "RET") 'cfml-indent-to-previous)
-  (setq indent-line-function 'sgml-indent-line)
-  (setq comment-start "<!---")
-  (setq comment-end " --->")
-  (setq-local comment-start-skip "<!---[ \t]*")
-  (setq-local comment-end-skip "[ \t]*---[ \t\n]*>")
-  (electric-indent-local-mode -1))
+(define-derived-mode cfml-tag-mode html-mode "CFML"
+  "Major mode for tag based cfml."
+  (setq-local sgml-empty-tags (append cfml-html-empty-tags cfml-empty-tags))
+  (setq-local sgml-unclosed-tags (append cfml-html-unclosed-tags cfml-unclosed-tags))
+  (setq tab-width cfml-tab-width)
+  (setq indent-line-function #'cfml-indent)
+)
+
+(with-no-warnings
+  (require 'mmm-mode))
+
+;;;###autoload
+(add-to-list 'magic-mode-alist
+             '("<cfcomponent" . cfml-tag-mode))
+;;;###autoload
+(add-to-list 'magic-mode-alist
+             '("<!---" . cfml-tag-mode))
+;;;###autoload
+(add-to-list 'auto-mode-alist
+             '("\\.cfm\\'" . cfml-tag-mode))
+;;;###autoload
+(add-to-list 'auto-mode-alist
+             '("\\.cfc\\'" . cfml-script-mode))
+
+(setq mmm-global-mode 'maybe)
+;;;###autoload
+(mmm-add-mode-ext-class nil "\\.cfm\\'" 'cfml-tag)
+;;;###autoload
+(mmm-add-mode-ext-class nil "\\.cfc\\'" 'cfml-tag)
+;;;###autoload
+(mmm-add-mode-ext-class nil "\\.cfm\\'" 'cfml-script)
+(setq mmm-submode-decoration-level 0)
+
+(mmm-add-classes
+ '((cfml-tag
+    :submode cfml-script-mode
+    :front "<cfscript>"
+    :back "[ \t]*</cfscript>")))
+
+(mmm-add-classes
+ '((cfml-script
+    :submode js-mode
+    :front "<script[^>]*>[ \t]*\n?"
+    :back "[ \t]*</script>")))
 
 (provide 'cfml-mode)
 

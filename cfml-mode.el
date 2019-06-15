03c6d8b682621dbed42ac57ca93f5b9bd458c667
Andrew Myers <amyers@rise-digital.com.au>
2019-06-16 00:34:53 +1000

    Removed dependency on mmm-mode

--- cfml-mode.el
+++ cfml-mode.el
@@ -36,22 +36,15 @@
 
 ;;; Code:
 
+(require 'mhtml-mode)
+(require 'js)
+
 (defgroup cfml nil
   "Major mode for cfml files"
   :prefix "cfml-"
   :group 'languages
   :link '(url-link :tag "Github" "https://github.com/am2605/cfml-mode")
   :link '(emacs-commentary-link :tag "Commentary" "cfml-mode"))
-
-(defcustom cfml-tab-width 4
-  "Tab width for cfml-mode."
-  :group 'cfml
-  :type 'integer)
-
-(defcustom cfml-extra-indent 0
-  "The number of columns added to every line's indentation."
-  :group 'cfml
-  :type 'integer)
 
 (defconst cfml-html-empty-tags
 	      '("area" "base" "basefont" "br" "col" "frame" "hr" "img" "input"
@@ -62,18 +55,10 @@
 		"p" "tbody" "td" "tfoot" "th" "thead" "tr"))
 
 (defconst cfml-empty-tags
-	'("cfargument" "cfdump" "cfinclude" "cfparam" "cfqueryparam" "cfset" "cfsetting" "cfthrow" ))
+	'("cfargument" "cfdump" "cfinclude" "cfparam" "cfqueryparam" "cfreturn" "cfset" "cfsetting" "cfthrow" ))
 	
 (defconst cfml-unclosed-tags
 	'("cfelse" "cfelseif"))
-
-(defun cfml-last-line-p ()
-  "Return whether point is on the last line in the buffer."
-  (save-excursion (= (line-number-at-pos) (line-number-at-pos (point-max)))))
-
-(defun cfml-line-empty-p ()
-  "Return whether point is on an empty line."
-  (string-match-p "^\s*$" (buffer-substring (point-at-bol) (point-at-eol))))
 
 (defun cfml-get-previous-indentation ()
   "Get the column of the previous indented line"
@@ -89,65 +74,114 @@
 	"\\(<cfelse\\(if[^>]+\\)?>\\)"
 )
 
-(defun cfml-indent ()
+(defconst cfml--cf-submode
+  (mhtml--construct-submode 'js-mode
+                            :name "cfscript"
+                            :end-tag "</cfscript>"
+                            :syntax-table js-mode-syntax-table
+                            :propertize #'js-syntax-propertize
+                            :keymap js-mode-map))
+
+(defun cfml-indent-line ()
   (interactive)
   (save-excursion
 	(beginning-of-line)
 	(skip-chars-forward " \t")
 	(cond
 	  ((looking-at cfml-outdent-regexp) (indent-line-to (max 0 (- (cfml-get-previous-indentation) cfml-tab-width))))
-	  (t (sgml-indent-line)))))
+	  (t (mhtml-indent-line)))))
+	 
+(defun cfml-syntax-propertize (start end)
+  ;; First remove our special settings from the affected text.  They
+  ;; will be re-applied as needed.
+  (remove-list-of-text-properties start end
+                                  '(syntax-table local-map mhtml-submode))
+  (goto-char start)
+  ;; Be sure to look back one character, because START won't yet have
+  ;; been propertized.
+  (unless (bobp)
+    (let ((submode (get-text-property (1- (point)) 'mhtml-submode)))
+      (if submode
+          (mhtml--syntax-propertize-submode submode end)
+        ;; No submode, so do what sgml-mode does.
+        (sgml-syntax-propertize-inside end))))
+  (funcall
+   (syntax-propertize-rules
+    ("<style.*?>"
+     (0 (ignore
+         (goto-char (match-end 0))
+         ;; Don't apply in a comment.
+         (unless (syntax-ppss-context (syntax-ppss))
+           (mhtml--syntax-propertize-submode mhtml--css-submode end)))))
+    ("<script.*?>"
+     (0 (ignore
+         (goto-char (match-end 0))
+         ;; Don't apply in a comment.
+         (unless (syntax-ppss-context (syntax-ppss))
+           (mhtml--syntax-propertize-submode mhtml--js-submode end)))))
+    ("<cfscript.*?>"
+     (0 (ignore
+         (goto-char (match-end 0))
+         ;; Don't apply in a comment.
+         (unless (syntax-ppss-context (syntax-ppss))
+           (mhtml--syntax-propertize-submode cfml--cf-submode end)))))		   
+    sgml-syntax-propertize-rules)
+	
+   ;; Make sure to handle the situation where
+   ;; mhtml--syntax-propertize-submode moved point.
+   (point) end))
+	 
 	 
 ;;;###autoload
-(define-derived-mode cfml-script-mode js-mode "cfscript"
-  (add-to-list 'mmm-save-local-variables 'js--quick-match-re)
-  (add-to-list 'mmm-save-local-variables 'js--quick-match-re-func))
+(define-derived-mode cfml-mode html-mode
+  '((sgml-xml-mode "XHTML+" "CFML+") (:eval (mhtml--submode-lighter)))
+  "Major mode based on `html-mode', but works with embedded JS and CSS.
 
-;;;###autoload
-(define-derived-mode cfml-tag-mode html-mode "CFML"
-  "Major mode for tag based cfml."
+Code inside a <script> element is indented using the rules from
+`js-mode'; and code inside a <style> element is indented using
+the rules from `css-mode'."
   (setq-local sgml-empty-tags (append cfml-html-empty-tags cfml-empty-tags))
   (setq-local sgml-unclosed-tags (append cfml-html-unclosed-tags cfml-unclosed-tags))
-  (setq tab-width cfml-tab-width)
-  (setq indent-line-function #'cfml-indent)
-)
+  (setq-local indent-line-function #'cfml-indent-line)
+  (setq-local syntax-propertize-function #'cfml-syntax-propertize)
+  (setq-local font-lock-fontify-region-function
+              #'mhtml--submode-fontify-region)
+  (setq-local font-lock-extend-region-functions
+              '(mhtml--extend-font-lock-region))
 
-(with-no-warnings
-  (require 'mmm-mode))
+  ;; Attach this to both pre- and post- hooks just in case it ever
+  ;; changes a key binding that might be accessed from the menu bar.
+  (add-hook 'pre-command-hook #'mhtml--pre-command nil t)
+  (add-hook 'post-command-hook #'mhtml--pre-command nil t)
+
+  ;; Make any captured variables buffer-local.
+  (mhtml--mark-buffer-locals mhtml--css-submode)
+  (mhtml--mark-buffer-locals mhtml--js-submode)
+  (mhtml--mark-buffer-locals cfml--cf-submode)
+
+  (mhtml--mark-crucial-buffer-locals mhtml--css-submode)
+  (mhtml--mark-crucial-buffer-locals mhtml--js-submode)
+  (mhtml--mark-crucial-buffer-locals cfml--cf-submode)
+  (setq mhtml--crucial-variables (delete-dups mhtml--crucial-variables))
+
+  ;: Hack
+  (js--update-quick-match-re)
+
+  ;; This is sort of a prog-mode as well as a text mode.
+  (run-hooks 'prog-mode-hook))
 
 ;;;###autoload
 (add-to-list 'magic-mode-alist
-             '("<cfcomponent" . cfml-tag-mode))
+             '("<cfcomponent" . cfml-mode))
 ;;;###autoload
 (add-to-list 'magic-mode-alist
-             '("<!---" . cfml-tag-mode))
+             '("<!---" . cfml-mode))
 ;;;###autoload
 (add-to-list 'auto-mode-alist
-             '("\\.cfm\\'" . cfml-tag-mode))
+             '("\\.cfm\\'" . cfml-mode))		
 ;;;###autoload
 (add-to-list 'auto-mode-alist
-             '("\\.cfc\\'" . cfml-script-mode))
-
-(setq mmm-global-mode 'maybe)
-;;;###autoload
-(mmm-add-mode-ext-class nil "\\.cfm\\'" 'cfml-tag)
-;;;###autoload
-(mmm-add-mode-ext-class nil "\\.cfc\\'" 'cfml-tag)
-;;;###autoload
-(mmm-add-mode-ext-class nil "\\.cfm\\'" 'cfml-script)
-(setq mmm-submode-decoration-level 0)
-
-(mmm-add-classes
- '((cfml-tag
-    :submode cfml-script-mode
-    :front "<cfscript>"
-    :back "[ \t]*</cfscript>")))
-
-(mmm-add-classes
- '((cfml-script
-    :submode js-mode
-    :front "<script[^>]*>[ \t]*\n?"
-    :back "[ \t]*</script>")))
+             '("\\.cfc\\'" . js-mode))
 
 (provide 'cfml-mode)
 
